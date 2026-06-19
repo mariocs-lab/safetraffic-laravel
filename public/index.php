fda83c1bd6cc66e1cd155993ea55be24ba6b6bc7
Mario Christian Choukrosimon <mario.chou.cs@gmail.com>
2026-06-19 12:32:53 +0800

    Fix: Menambahkan env variabel untuk mem-bypass cache bootstrap Vercel

--- public/index.php
+++ public/index.php
@@ -15,18 +15,34 @@ require __DIR__ . '/../vendor/autoload.php';
 // 2. Panggil bootstrap Laravel (membuat instance $app)
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 3. --- HACK VERCEL: Pindahkan folder storage ke /tmp ---
+// 3. --- HACK VERCEL EXODIA: Pindahkan folder storage & cache ke /tmp ---
 $storage = '/tmp/storage';
-$dirs = ['app', 'framework/views', 'framework/cache', 'framework/sessions', 'logs'];
+$dirs = [
+    'app',
+    'framework/views',
+    'framework/cache',
+    'framework/cache/data',
+    'framework/sessions',
+    'logs',
+    'bootstrap/cache'
+];
 foreach ($dirs as $dir) {
     if (!is_dir($storage . '/' . $dir)) {
         mkdir($storage . '/' . $dir, 0777, true);
     }
 }
 $app->useStoragePath($storage);
-// ------------------------------------------------------
 
-// 4. Jalankan aplikasi (Mendukung Laravel 10 & 11)
+// Paksa rute cache dasar ke /tmp agar bebas dari kutukan Read-Only Vercel
+putenv('APP_SERVICES_CACHE=' . $storage . '/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=' . $storage . '/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=' . $storage . '/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=' . $storage . '/bootstrap/cache/routes.php');
+putenv('APP_EVENTS_CACHE=' . $storage . '/bootstrap/cache/events.php');
+putenv('VIEW_COMPILED_PATH=' . $storage . '/framework/views');
+// ----------------------------------------------------------------------
+
+// 4. Jalankan aplikasi
 if (method_exists($app, 'handleRequest')) {
     $app->handleRequest(Request::capture());
 } else {
